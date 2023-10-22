7e871578160bf584c0c7cf1189b314fbbd9f15d0
miguel-l-b <miguellopesbraido@gmail.com>
2023-10-21 21:04:54 -0300

    test: testing toString()

--- src/App.java
+++ src/App.java
@@ -6,7 +6,15 @@ public class App {
     public static void main(String[] args) {
         try {
             BinarySearchTree<Integer> tree = new BinarySearchTree<>();
-            System.out.println(tree.size());
+            Random randomGenerate = new Random();
+
+            System.out.println("incluindo...");
+            for(int i = 0; i < 100000; i++) tree.insert(randomGenerate.nextInt());
+
+            System.out.println("incluindo!");
+            long init = System.currentTimeMillis();
+            System.out.println(tree);
+            System.out.println("ToString() demorou: "+(System.currentTimeMillis()-init)+"ms");
         } catch (Exception e) {
             e.printStackTrace();
         }
